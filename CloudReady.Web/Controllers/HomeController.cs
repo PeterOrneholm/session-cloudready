e40ee00317a5f607973c0bde7a071e62eec15ea6
Peter Örneholm <peter@orneholm.com>
2016-01-31 16:15:18 +0100

    Cleaned and added simple list

--- CloudReady.Web/Controllers/HomeController.cs
+++ CloudReady.Web/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
+using CloudReady.Web.Models;
 
 namespace CloudReady.Web.Controllers
 {
@@ -10,21 +11,49 @@ namespace CloudReady.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var images = new List<Image>();
+
+            using (var connection = new SqlConnection("Data Source=.;Initial Catalog=CloudReady;Trusted_Connection=True"))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Images";
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = (int)reader["Id"];
+                        var extension = (string)reader["Extension"];
+
+                        images.Add(new Image
+                        {
+                            Id = id,
+                            Extension = extension,
+                            Name = (string)reader["Name"],
+                            Size = (int)reader["Size"],
+                            Url = $"/fileuploads/{id}.{extension}"
+                        });
+                    }
+                }
+            }
+
+            return View(images);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult UploadImages(IEnumerable<HttpPostedFileBase> files)
         {
-            ViewBag.Message = "Your application description page.";
+            foreach (var file in files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/fileuploads/"), fileName);
+                    file.SaveAs(path);
+                }
+            }
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ CloudReady.Web/Models/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudReady.Web.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Extension { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Url { get; set; }
+    }
+}
